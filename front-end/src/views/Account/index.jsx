3c38070c039a4fdfc4cbb9b52ffc0f95222e52e8
yohanesam <yohanesam@users.noreply.github.com>
2019-06-24 11:03:35 +0700

    edit user done

--- front-end/src/views/Account/index.jsx
+++ front-end/src/views/Account/index.jsx
@@ -14,6 +14,7 @@ import DashboardLayout from '../../layouts/Dashboard';
 
 // Custom components
 import AccountProfile from './components/AccountProfile';
+import AccountOverview from './components/AccountOverview';
 import AccountDetails from './components/AccountDetails';
 
 // Component styles
