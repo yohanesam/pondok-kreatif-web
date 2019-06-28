d26b49ce1f0f9d550af2cf2f96fe5ccd9dba99d3
yohanesam <yohanesam@users.noreply.github.com>
2019-06-28 07:13:15 +0700

    change api base url

--- front-end/src/config/Apis.js
+++ front-end/src/config/Apis.js
@@ -1,5 +1,5 @@
 // Configuration for backend app
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "http://peka.sharkhomedesign.com/";
 const UMKM_REGISTER_URI = "api/umkm/";
 const LOGIN_URI = "api/login/";
 const PROFILE_URI = "api/umkm/";
